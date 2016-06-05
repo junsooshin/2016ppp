3eed5692dd0f3c477644e7ae72a25796f222e25f
Jun Soo Shin <jsshin41@gmail.com>
2016-06-05 10:48:35 +0200

    implemented points system - logic needs to be rechecked b/c a slight difference in serv. prob. vastly changes win prob.

--- week5and6/tennis.scala
+++ week5and6/tennis.scala
@@ -4,16 +4,21 @@ import com.cra.figaro.algorithm.factored.VariableElimination
 
 object tennisExercise {
 
-	val probP1Serve = 0.7
-	val probP2Serve = 0.5
+	val probP1Serve = 0.58
+	val probP2Serve = 0.55
 
 	val Match: Element[Boolean] = mkMatch(0, 0)  // starts the match
 
 	def mkMatch(p1Sets: Int, p2Sets: Int): Element[Boolean] = {
-		// println(p1Sets + "-" + p2Sets)
-
-		if (p1Sets == 2) Constant(true)
-		else if (p2Sets == 2) Constant(false)
+		// if ((p1Sets == 0) && (p2Sets == 0)) println("NEW MATCH")
+		if (p1Sets == 2) {
+			// println("P1 Wins " + p1Sets + "-" + p2Sets)
+			// println("##############################")
+			Constant(true)}
+		else if (p2Sets == 2) {
+			// println("P2 Wins " + p1Sets + "-" + p2Sets)
+			// println("##############################")
+			Constant(false)}
 		else for {
 			server <- Flip(0.5)  // this might not alternate server on a new set
 			setResult <- mkSet(0, 0, p1Sets + p2Sets + 1, server)  // calls mkSet() and gets the result when the set is over
@@ -23,12 +28,13 @@ object tennisExercise {
 	}
 
 	def mkSet(p1Games: Int, p2Games: Int, setNum: Int, server: Boolean): Element[Boolean] = {
-		// if (server) println("--- P1 serving (" + p1Games + ":" + p2Games + ") ---")
-		// else println("--- P2 serving (" + p1Games + ":" + p2Games + ") ---")
-
-		if ((setNum != 3) && (p1Games == 6) && (p2Games == 6)) tiebreak(0, 0, server)
-		else if ((p1Games >= 6) && ((p1Games - p2Games) >= 2)) Constant(true)
-		else if ((p2Games >= 6) && ((p2Games - p1Games) >= 2)) Constant(false)
+		if ((setNum != 3) && (p1Games == 6) && (p2Games == 6)) tiebreak(0, 0, setNum, server)
+		else if ((p1Games >= 6) && ((p1Games - p2Games) >= 2)) {
+			// println("Set " + setNum + " Completed " + p1Games + ":" + p2Games)
+			Constant(true)}
+		else if ((p2Games >= 6) && ((p2Games - p1Games) >= 2)) {
+			// println("Set " + setNum + " Completed " + p1Games + ":" + p2Games)
+			Constant(false)}
 		else for {
 				  gameResult <- mkGame(0, 0, server)
 				  nextGame <- if (gameResult) mkSet(p1Games + 1, p2Games, setNum, !server)
@@ -41,21 +47,31 @@ object tennisExercise {
 	 *		 The first server only serves the first point, and then for the rest of the tiebreak,
 	 *       the players alternate serves for 2 points
 	 */
-	def tiebreak(p1Points: Int, p2Points: Int, server: Boolean): Element[Boolean] = {
+	def tiebreak(p1Points: Int, p2Points: Int, setNum: Int, server: Boolean): Element[Boolean] = {
 		val nextServer = if (((p1Points + p2Points) % 2) == 1) !server
-							 else server
-		// println(">>TIEBREAK<< " + nextServer + " serving")
-
+						 else server
 		if ((p1Points >= 7) && ((p1Points - p2Points) >= 2)) {
-			// println("hi")
+			// println("Set " + setNum + " Completed 7:6 (" + p1Points + ":" + p2Points + ")")
 			Constant(true)}  
-		else if ((p2Points >= 7) && ((p2Points - p1Points) >= 2)) Constant(false)
+		else if ((p2Points >= 7) && ((p2Points - p1Points) >= 2)) {
+			// println("Set " + setNum + " Completed 6:7 (" + p1Points + ":" + p2Points + ")")
+			Constant(false)}
 		else for {
 				pointResult <- Flip( if (nextServer) probP1Serve else probP2Serve )
 				// dummy = println("hello")
-				nextPoint <- if (pointResult) tiebreak(p1Points + 1, p2Points, nextServer)
-						     else tiebreak(p1Points, p2Points + 1, nextServer)
+				nextPoint <- if (pointResult) tiebreak(p1Points + 1, p2Points, setNum, nextServer)
+						     else tiebreak(p1Points, p2Points + 1, setNum, nextServer)
 			} yield nextPoint
+	}
+
+	def mkGame(p1Points: Int, p2Points: Int, server: Boolean): Element[Boolean] = {
+		if ((p1Points >= 4) && ((p1Points - p2Points) >= 2)) Constant(true)
+		else if ((p2Points >= 4) && ((p2Points - p1Points) >= 2)) Constant(false)
+		else for {
+			pointResult <- Flip( if (server) probP1Serve else probP2Serve )
+			nextPoint <- if (pointResult) mkGame(p1Points + 1, p2Points, server)
+				         else mkGame(p1Points, p2Points + 1, server)
+		} yield nextPoint
 	}
 
 	def main(args: Array[String]) {
