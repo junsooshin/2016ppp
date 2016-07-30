2697c711cfc250f9dbaa422202537c8c3e278b8e
Jun Soo Shin <jsshin41@gmail.com>
2016-07-31 01:25:47 +0200

    checked for the right names on the lineups and put names on the arrays and variables

--- final project/baseball.scala
+++ final project/baseball.scala
@@ -11,6 +11,7 @@ import com.cra.figaro.language._
 import com.cra.figaro.library.compound._
 import java.io._  // for writing to a file
 import scala.io.StdIn.{readLine}  // for prompting user input
+import util.control.Breaks._  // lets us use "breakable" and break" method
 
 object BaseballSimulator {
 
@@ -20,61 +21,49 @@ object BaseballSimulator {
 	val pitching = ArrayBuffer[Array[String]]()
 	val league = ArrayBuffer[Array[String]]()
 
-	abstract class player {
-		val name: String
-		val p1B: Double
-		val p2B: Double
-		val p3B: Double
-		val pHR: Double
-		val pBB: Double
-		val pIBB: Double
-		val pHBP: Double
-		val pSO: Double
-		val pSF: Double
-		val pSH: Double
-		val pNO: Double
+	class player {
+		var name: String = null
+		var p1B: Double = 0
+		var p2B: Double = 0
+		var p3B: Double = 0
+		var pHR: Double = 0
+		var pBB: Double = 0
+		var pIBB: Double = 0
+		var pHBP: Double = 0
+		var pSO: Double = 0
+		var pSF: Double = 0
+		var pSH: Double = 0
+		var pNO: Double = 0
 	}
-	abstract class batter extends player ()
-	abstract class pitcher extends player ()
-	abstract class league extends player ()
 
 	def checkArgsLength(args: Array[String]) {
 		if (args.length == 0) {
-			println()
-			println("No argument given")
-			println()
+
+			println("\nNo argument given\n")
 			println("Type in two lineups as arguments; one team as one quote, "
 					+ "players' names separated by commas, and pitcher's name "
-					+ "as 10th player.")
-			println()
+					+ "as 10th player.\n")
 			println("Ex: run \"Dexter Fowler, Kris Bryant,..., Jon Lester\" "
-					+ "\"Mookie Betts, Dustin Pedroia,..., David Price\"")
-			println()
+					+ "\"Mookie Betts, Dustin Pedroia,..., David Price\"\n")
 			System.exit(0)
 		} else if (args.length == 1) {
-			println()
-			println("Only one argument given") 
-			println()
+			println("\nOnly one argument given\n")
 			println("Type in two lineups as arguments; one team as one quote, "
 					+ "players' names separated by commas, and pitcher's name "
-					+ "as 10th player.")
+					+ "as 10th player.\n")
 			println("Ex: run \"Dexter Fowler, Kris Bryant,..., Jon Lester\" "
-					+ "\"Mookie Betts, Dustin Pedroia,..., David Price\"")
-			println()
+					+ "\"Mookie Betts, Dustin Pedroia,..., David Price\"\n")
 			System.exit(0)
 		} else if (args.length == 2) {
 			createDatabases()
 			checkValidNames(args)
 		} else {
-			println()
-			println("Too many arguments given")
-			println()
+			println("\nToo many arguments given\n")
 			println("Type in two lineups as arguments; one team as one quote, "
 					+ "players' names separated by commas, and pitcher's name "
-					+ "as 10th player.")
+					+ "as 10th player.\n")
 			println("Ex: run \"Dexter Fowler, Kris Bryant,..., Jon Lester\" "
-					+ "\"Mookie Betts, Dustin Pedroia,..., David Price\"")
-			println()
+					+ "\"Mookie Betts, Dustin Pedroia,..., David Price\"\n")
 			System.exit(0)
 		}
 	}
@@ -126,11 +115,88 @@ object BaseballSimulator {
 		// // pw.close()
 	}
 
+	// This function first checks for the right number of players on the 
+	// lineups and for the correct names. Then, it creates arrays for batters
+	// and values for pitchers
 	def checkValidNames(args: Array[String]) {
-		println(args.split(","))
-		// for (numRow <- 0 until batting.length) {
+		val tmpLineup1 = args(0).split(",").map(_.trim)
+		val tmpLineup2 = args(1).split(",").map(_.trim)
+		// println(tmpLineup1.length)
+		// println(tmpLineup2.length)
 
-		// }
+		if (tmpLineup1.length != 10) {
+			println("\nNeeded 10 players on the 1st lineup but received "
+					+ tmpLineup1.length)
+			System.exit(0)
+		}
+		if (tmpLineup2.length != 10) {
+			println("\nNeeded 10 players on the 2nd lineup but received "
+					+ tmpLineup2.length)
+			System.exit(0)
+		}
+
+		val batters1 = Array.fill(9)(new player)
+		val pitcher1 = new player
+		val batters2 = Array.fill(9)(new player)
+		val pitcher2 = new player
+
+		for (order <- 0 until tmpLineup1.length - 1) {
+			breakable {
+				for (numRow <- 0 until batting.length) {
+					if (batting(numRow)(0) == tmpLineup1(order)) {
+						batters1(order).name = tmpLineup1(order)
+						break
+					} else if (numRow == batting.length - 1) {
+						// we just checked the last row and did not find the player
+						println("\nPlayer named " + "\"" + tmpLineup1(order) 
+						+ "\" was not found")
+						System.exit(0)
+					}
+				}
+			}
+		}
+
+		breakable {
+			for (numRow <- 0 until pitching.length) {
+				if (pitching(numRow)(0) == tmpLineup1(9)) {
+					pitcher1.name = tmpLineup1(9)
+					break
+				} else if (numRow == pitching.length - 1) {
+					println("\nPlayer named " + "\"" + tmpLineup1(9) 
+					+ "\" was not found")
+					System.exit(0)
+				}
+			}
+		}
+
+		for (order <- 0 until tmpLineup2.length - 1) {
+			breakable {
+				for (numRow <- 0 until batting.length) {
+					if (batting(numRow)(0) == tmpLineup2(order)) {
+						batters2(order).name = tmpLineup2(order)
+						break
+					} else if (numRow == batting.length - 1) {
+						// we just checked the last row and did not find the player
+						println("\nPlayer named " + "\"" + tmpLineup2(order) 
+						+ "\" was not found")
+						System.exit(0)
+					}
+				}
+			}
+		}
+
+		breakable {
+			for (numRow <- 0 until pitching.length) {
+				if (pitching(numRow)(0) == tmpLineup2(9)) {
+					pitcher2.name = tmpLineup2(9)
+					break
+				} else if (numRow == pitching.length - 1) {
+					println("\nPlayer named " + "\"" + tmpLineup2(9) 
+					+ "\" was not found")
+					System.exit(0)
+				}
+			}
+		}
 	}
 
 	def main(args: Array[String]) {
