1b2163d2ea240691e9136d0a87657a1afc922755
Jun Soo Shin <jsshin41@gmail.com>
2016-08-03 16:39:28 +0200

    finished the bottom 9th situation simulation

--- final project/baseball3.scala
+++ final project/baseball3.scala
@@ -13,6 +13,12 @@ import com.cra.figaro.language._
 import com.cra.figaro.library.compound._
 import util.control.Breaks._  // for "breakable" and break" method
 import com.cra.figaro.algorithm.sampling.Importance
+import com.cra.figaro.algorithm.factored.MPEVariableElimination
+import com.cra.figaro.algorithm.factored.beliefpropagation.MPEBeliefPropagation
+import com.cra.figaro.algorithm.sampling.MetropolisHastingsAnnealer
+import com.cra.figaro.algorithm.sampling.ProposalScheme
+import com.cra.figaro.algorithm.sampling.Schedule
+
 
 object BaseballSimulator {
 
@@ -263,10 +269,11 @@ object BaseballSimulator {
 	def playGame(currBatterOrder: Int): Element[(scorecard, scorecard, Boolean)] = {
 		val initState1 = scorecard(batters1, pitcher2, 0, 0, "000", -1, -1, -1, 0)
 		// setting up the bot 9th bases loaded situation
+		// 4th batter on team 2 at the plate
 		def getRunnerOrder (currBatterOrder: Int, base: Int): Int = {
 			(currBatterOrder + 9 - base) % 9
 		}
-		val initState2 = scorecard(batters2, pitcher1, 0, 1, "111", 1, 2, 3, currBatterOrder)
+		val initState2 = scorecard(batters2, pitcher1, 0, 1, "111", getRunnerOrder(currBatterOrder, 1), getRunnerOrder(currBatterOrder, 2), getRunnerOrder(currBatterOrder, 3), currBatterOrder)
 
 		val pair = playInnings(initState1, initState2, 8.5)
 
@@ -288,8 +295,9 @@ object BaseballSimulator {
 
 		var inning = inn
 
-		println("Inning: " + inning)
-		println("Score: " + currState1.runs + "-" + currState2.runs)
+		// println()
+		// println("Inning: " + inning)
+		// println("Score: " + currState1.runs + "-" + currState2.runs)
 
 		if (inning <= 8) {  // top 9th isn't completed yet
 			if (inning - (inning.floor) == 0) {  // top half; team 1 offense
@@ -319,6 +327,7 @@ object BaseballSimulator {
 				} yield pair
 			}
 		} else {  // bot 9th is completed
+			// println("\n--- GAME OVER ---\n")
 			Constant(currState1, currState2)
 		}
 		// else {  // bot 9th or more is completed; TODO: end the game as soon as the home team is up
@@ -351,50 +360,82 @@ object BaseballSimulator {
 		var oppPitcher = currState.oppPitcher
 		var runs = currState.runs
 		var outs = currState.outs
+		var bases = currState.bases
 		var base1 = currState.base1
 		var base2 = currState.base2
 		var base3 = currState.base3
 		var currBatterOrder = currState.currBatterOrder
 
-		if (runs > 0) {
-			Constant(currState)
-		}
+		// if (outs >= 3) {
+		// 	Constant(currState)  // home team did not score; game is a draw
+		// }
 
-		var bases = currState.bases
 		var currPlay = playAtBat(currState)
 
 		val pFirstToThird = 2455.0 / 8879.0   // on a single; otherwise, first to second
 		val pSecondToHome = 2931.0 / 5104.0   // on a single; otherwise, second to third
 		val pFirstToHome  = 1051.0 / 2425.0   // on a double; otherwise, first to third
 
-		batters(currBatterOrder).tPA += 1
-
 		def getNewBatterOrder(currBatterOrder: Int): Int = {
-			if (currBatterOrder == 8) 0
+			if (currBatterOrder >= 8) 0
 			else currBatterOrder + 1
 		}
 
 		for {
 			play <- currPlay
-
-			newBases <- if (play == "1B")
+			newBases <- if (play == "1B") {
+							// println("batter " + currBatterOrder + " -> Play: " + play)
 							Select(pSecondToHome -> "110",
 							   	   pSecondToHome * pFirstToThird -> "101",
 							       1 - (pSecondToHome * pFirstToThird) -> "111")
-					    else if (play == "2B")
+					    } else if (play == "2B") {
+					    	// println("batter " + currBatterOrder + " -> Play: " + play)
 					    	Select(pFirstToHome -> "010", 1 - pFirstToHome -> "011")
-					    else if (play == "3B")
+					    } else if (play == "3B") {
+					    	// println("batter " + currBatterOrder + " -> Play: " + play)
 					    	Constant("001")
-					    else if (play == "HR")
+					    } else if (play == "HR") {
+							// println("batter " + currBatterOrder + " -> Play: " + play)
 					    	Constant("000")
-					    else if (play == "TW")
+					    } else if (play == "TW") {
+					    	// println("batter " + currBatterOrder + " -> Play: " + play)
 					    	Constant("111")
-					    else if (play == "SO")
+					    } else if (play == "SO") {
+					    	// println("batter " + currBatterOrder + " -> Play: " + play)
 					    	Constant("111")
-					    else
-					    	Select(0.4 -> "110", 0.1 -> "101", 0.5 -> "111")
+					    } else { // (play == "BO") 
+					    	// println("batter " + currBatterOrder + " -> Play: " + play)
+					    	Select(0.3 -> "110", 0.1 -> "101", 0.6 -> "111")
+					    }
+
+			recurse <- if (play == "SO") {
+							val newBatterOrder = getNewBatterOrder(currBatterOrder)
+							val newPA = batters(currBatterOrder).tPA + 1
+							val newOuts = outs + 1
+							val newSO = batters(currBatterOrder).tSO + 1
+							val newCurrBatter = batters(currBatterOrder).copy(tPA = newPA, tSO = newSO)
+							val newBatters = batters.updated(currBatterOrder, newCurrBatter)
+							val newState = currState.copy(outs = newOuts, batters = newBatters, currBatterOrder = newBatterOrder)
+							if (newOuts >= 3) {
+								Constant(newState)
+							} else {
+								playHalfInning(newState)
+							}
+					   } else if ((play == "BO") && (newBases == "111")) {
+					   		val newBatterOrder = getNewBatterOrder(currBatterOrder)
+							val newPA = batters(currBatterOrder).tPA + 1
+					   		val newOuts = outs + 1
+					   		val newBO = batters(currBatterOrder).tBO + 1
+							val newCurrBatter = batters(currBatterOrder).copy(tPA = newPA, tBO = newBO)
+							val newBatters = batters.updated(currBatterOrder, newCurrBatter)
+							val newState = currState.copy(outs = newOuts, batters = newBatters, currBatterOrder = newBatterOrder)
+							playHalfInning(newState)
+					   } else {  // not going to fall in this case
+					   		Constant(currState)
+					   }
 		} yield {
 			val newBatterOrder = getNewBatterOrder(currBatterOrder)
+			val newPA = batters(currBatterOrder).tPA + 1
 			if (play == "1B") {
 				val newBase1 = currBatterOrder
 				val new1B = batters(currBatterOrder).t1B + 1
@@ -406,7 +447,7 @@ object BaseballSimulator {
 					val newBase2 = base1
 					val newRun2nd = batters(base2).tR + 1
 					val newRBI = batters(currBatterOrder).tRBI + 2
-					val newCurrBatter = batters(currBatterOrder).copy(t1B = new1B, tRBI = newRBI)
+					val newCurrBatter = batters(currBatterOrder).copy(tPA = newPA, t1B = new1B, tRBI = newRBI)
 					val newRunner2nd = batters(base2).copy(tR = newRun2nd)
 					val newBatters = batters.updated(currBatterOrder, newCurrBatter)
 					val newNewBatters = newBatters.updated(base2, newRunner2nd)
@@ -418,7 +459,7 @@ object BaseballSimulator {
 					val newBase2 = -1
 					val newRun2nd = batters(base2).tR + 1
 					val newRBI = batters(currBatterOrder).tRBI + 2
-					val newCurrBatter = batters(currBatterOrder).copy(t1B = new1B, tRBI = newRBI)
+					val newCurrBatter = batters(currBatterOrder).copy(tPA = newPA, t1B = new1B, tRBI = newRBI)
 					val newRunner2nd = batters(base2).copy(tR = newRun2nd)
 					val newBatters = batters.updated(currBatterOrder, newCurrBatter)
 					val newNewBatters = newBatters.updated(base3, newRunner3rd)
@@ -429,10 +470,10 @@ object BaseballSimulator {
 					val newBase3 = base2
 					val newBase2 = base1
 					val newRBI = batters(currBatterOrder).tRBI + 1
-					val newCurrBatter = batters(currBatterOrder).copy(t1B = new1B, tRBI = newRBI)
+					val newCurrBatter = batters(currBatterOrder).copy(tPA = newPA, t1B = new1B, tRBI = newRBI)
 					val newBatters = batters.updated(currBatterOrder, newCurrBatter)
 					val newNewBatters = newBatters.updated(base3, newRunner3rd)
-					currState.copy(bases = newBases, runs = newRuns, base1 = newBase1, base2 = newBase2, base3 = newBase3, batters = newNewBatters, currBatterOrder = newBatterOrder)
+					currState.copy(runs = newRuns, base1 = newBase1, base2 = newBase2, base3 = newBase3, batters = newNewBatters, currBatterOrder = newBatterOrder)
 				}
 			} else if (play == "2B") {
 				val newBase2 = currBatterOrder
@@ -448,7 +489,7 @@ object BaseballSimulator {
 					val newRun1st = batters(base1).tR + 1
 					val newRunner1st = batters(base1).copy(tR = newRun1st)
 					val newRBI = batters(currBatterOrder).tRBI + 3
-					val newCurrBatter = batters(currBatterOrder).copy(t2B = new2B, tRBI = newRBI)
+					val newCurrBatter = batters(currBatterOrder).copy(tPA = newPA, t2B = new2B, tRBI = newRBI)
 					val newBatters = batters.updated(currBatterOrder, newCurrBatter)
 					val newNewBatters = newBatters.updated(base3, newRunner3rd)
 					val newNewNewBatters = newNewBatters.updated(base2, newRunner2nd)
@@ -458,7 +499,7 @@ object BaseballSimulator {
 					val newRuns = runs + 2
 					val newBase3 = base1
 					val newRBI = batters(currBatterOrder).tRBI + 2
-					val newCurrBatter = batters(currBatterOrder).copy(t2B = new2B, tRBI = newRBI)
+					val newCurrBatter = batters(currBatterOrder).copy(tPA = newPA, t2B = new2B, tRBI = newRBI)
 					val newBatters = batters.updated(currBatterOrder, newCurrBatter)
 					val newNewBatters = newBatters.updated(base3, newRunner3rd)
 					val newNewNewBatters = newNewBatters.updated(base2, newRunner2nd)
@@ -477,7 +518,7 @@ object BaseballSimulator {
 				val newRunner1st = batters(base1).copy(tR = newRun1st)
 				val new3B = batters(currBatterOrder).t3B + 1
 				val newRBI = batters(currBatterOrder).tRBI + 3
-				val newCurrBatter = batters(currBatterOrder).copy(t3B = new3B, tRBI = newRBI)
+				val newCurrBatter = batters(currBatterOrder).copy(tPA = newPA, t3B = new3B, tRBI = newRBI)
 				val newBatters = batters.updated(currBatterOrder, newCurrBatter)
 				val newNewBatters = newBatters.updated(base3, newRunner3rd)
 				val newNewNewBatters = newNewBatters.updated(base2, newRunner2nd)
@@ -497,7 +538,7 @@ object BaseballSimulator {
 				val newHR = batters(currBatterOrder).tHR + 1
 				val newRBI = batters(currBatterOrder).tRBI + 4
 				val newR = batters(currBatterOrder).tR + 1
-				val newCurrBatter = batters(currBatterOrder).copy(tHR = newHR, tRBI = newRBI, tR = newR)
+				val newCurrBatter = batters(currBatterOrder).copy(tPA = newPA, tHR = newHR, tRBI = newRBI, tR = newR)
 				val newBatters = batters.updated(currBatterOrder, newCurrBatter)
 				val newNewBatters = newBatters.updated(base3, newRunner3rd)
 				val newNewNewBatters = newNewBatters.updated(base2, newRunner2nd)
@@ -512,41 +553,46 @@ object BaseballSimulator {
 				val newRunner3rd = batters(base3).copy(tR = newRun3rd)
 				val newTW = batters(currBatterOrder).tTW + 1
 				val newRBI = batters(currBatterOrder).tRBI + 1
-				val newCurrBatter = batters(currBatterOrder).copy(tTW = newTW, tRBI = newRBI)
+				val newCurrBatter = batters(currBatterOrder).copy(tPA = newPA, tTW = newTW, tRBI = newRBI)
 				val newBatters = batters.updated(currBatterOrder, newCurrBatter)
 				val newNewBatters = newBatters.updated(base3, newRunner3rd)
-				currState.copy(bases = newBases, runs = newRuns, base1 = newBase1, base2 = newBase2, base3 = newBase3, batters = newNewBatters, currBatterOrder = newBatterOrder)
+				currState.copy(runs = newRuns, base1 = newBase1, base2 = newBase2, base3 = newBase3, batters = newNewBatters, currBatterOrder = newBatterOrder)
 			} else if (play == "SO") {
-				val newOuts = outs + 1
-				val newSO = batters(currBatterOrder).tSO + 1
-				val newCurrBatter = batters(currBatterOrder).copy(tSO = newSO)
 				// update currState and recurse
+				recurse
 			} else {  // (play == "BO")
 				val newOuts = outs + 1
 				val newBO = batters(currBatterOrder).tBO + 1
-				if (newBases == "110") {
-					val newRuns = runs + 1
-					val newBase3 = -1
-					val newRun3rd = batters(base3).tR + 1
-					val newRunner3rd = batters(base3).copy(tR = newRun3rd)
-					val newRBI = batters(currBatterOrder).tRBI + 1
-					val newCurrBatter = batters(currBatterOrder).copy(tBO = newBO, tRBI = newRBI)
+				if (newOuts >= 3) {
+					val newCurrBatter = batters(currBatterOrder).copy(tPA = newPA, tBO = newBO)
 					val newBatters = batters.updated(currBatterOrder, newCurrBatter)
-					val newNewBatters = newBatters.updated(base3, newRunner3rd)
-					currState.copy(bases = newBases, runs = newRuns, base3 = newBase3, batters = newNewBatters, currBatterOrder = newBatterOrder )
-				} else if (newBases == "101") {
-					val newRuns = runs + 1
-					val newBase3 = base2
-					val newBase2 = -1
-					val newRun3rd = batters(base3).tR + 1
-					val newRunner3rd = batters(base3).copy(tR = newRun3rd)
-					val newRBI = batters(currBatterOrder).tRBI + 1
-					val newCurrBatter = batters(currBatterOrder).copy(tBO = newBO, tRBI = newRBI)
-					val newBatters = batters.updated(currBatterOrder, newCurrBatter)
-					val newNewBatters = newBatters.updated(base3, newRunner3rd)
-					currState.copy(bases = newBases, runs = newRuns, base3 = newBase3, base2 = newBase2, batters = newNewBatters, currBatterOrder = newBatterOrder )
-				} else {  // (newBases == "111")
-					// update currState and recurse
+					currState.copy(bases = newBases, batters = newBatters, currBatterOrder = newBatterOrder)
+				} else {
+					if (newBases == "110") {
+						val newRuns = runs + 1
+						val newBase3 = -1
+						val newRun3rd = batters(base3).tR + 1
+						val newRunner3rd = batters(base3).copy(tR = newRun3rd)
+						val newRBI = batters(currBatterOrder).tRBI + 1
+						val newCurrBatter = batters(currBatterOrder).copy(tPA = newPA, tBO = newBO, tRBI = newRBI)
+						val newBatters = batters.updated(currBatterOrder, newCurrBatter)
+						val newNewBatters = newBatters.updated(base3, newRunner3rd)
+						currState.copy(bases = newBases, runs = newRuns, base3 = newBase3, batters = newNewBatters, currBatterOrder = newBatterOrder)
+					} else if (newBases == "101") {
+						val newRuns = runs + 1
+						val newBase3 = base2
+						val newBase2 = -1
+						val newRun3rd = batters(base3).tR + 1
+						val newRunner3rd = batters(base3).copy(tR = newRun3rd)
+						val newRBI = batters(currBatterOrder).tRBI + 1
+						val newCurrBatter = batters(currBatterOrder).copy(tPA = newPA, tBO = newBO, tRBI = newRBI)
+						val newBatters = batters.updated(currBatterOrder, newCurrBatter)
+						val newNewBatters = newBatters.updated(base3, newRunner3rd)
+						currState.copy(bases = newBases, runs = newRuns, base3 = newBase3, base2 = newBase2, batters = newNewBatters, currBatterOrder = newBatterOrder)
+					} else {  // (newBases == "111")
+						// update currState and recurse
+						recurse
+					}
 				}
 			}
 		} // end of yield
@@ -604,7 +650,7 @@ object BaseballSimulator {
 	/*	This function prints out the box score after a game is completed
 	 */
 	def printBoxScore(scorecard1: scorecard, scorecard2: scorecard) {
-		helper(scorecard1, 1)
+		// helper(scorecard1, 1)
 		helper(scorecard2, 2)
 		def helper(card: scorecard, n: Int) {
 			var batters = card.batters
@@ -625,28 +671,95 @@ object BaseballSimulator {
 		}
 	}
 
+	def runAlgorithms(triple: Element[(scorecard, scorecard, Boolean)]) {
+
+		val team2Wins = triple._3
+		println("\n############################################################")
+		println("Running Importance Sampling algorithm ...")
+		println("\nQ1 -> Win probability of team 2 winning, when the game is tied, "
+				+ "bases are loaded with 1 out, and the No.4 batter is up:")
+		// for {
+		// 	results <- triple
+		// } yield {
+		// 	printBoxScore(results._1, results._2)
+		// }
+		val imp = Importance(100000, team2Wins)
+		imp.start()
+		println("\n   " + imp.probability(team2Wins, true))
+		imp.kill()
+
+		team2Wins.observe(true)
+
+  		val b3RBI = for {
+	  					card <- triple._2
+	  				} yield {
+	  					if (card.batters(3).tRBI > 0) true
+	  					else false
+	  				}
+	  	val b4RBI = for {
+	  						card <- triple._2
+	  					} yield {
+	  						if (card.batters(4).tRBI > 0) true
+	  						else false
+	  					}
+  		val b5RBI = for {
+  						card <- triple._2
+  					} yield {
+  						if (card.batters(5).tRBI > 0) true
+  						else false
+  					}
+
+  		val b3tRBI = for {
+	  					card <- triple._2
+	  				} yield {
+	  					card.batters(3).tRBI.toDouble
+	  				}
+	  	val b4tRBI = for {
+	  					card <- triple._2
+	  				} yield {
+	  					card.batters(4).tRBI.toDouble
+	  				}
+  		val b5tRBI = for {
+  						card <- triple._2
+  					} yield {
+						card.batters(5).tRBI.toDouble
+  					}
+
+  		println("\nQ2 -> Given that team 2 won the game, ")
+	  	val imp2 = Importance(100000, b3RBI, b4RBI, b5RBI, b3tRBI, b4tRBI, b5tRBI)
+	  	imp2.start()
+	  	println("\n   probability that batter(3) "
+	  			+ "got the game-winning RBI: " + imp2.probability(b3RBI, true))
+	  	println("\n   probability that batter(4) "
+	  			+ "got the game-winning RBI: " + imp2.probability(b4RBI, true))
+		println("\n   probability that batter(5) "
+	  			+ "got the game-winning RBI: " + imp2.probability(b5RBI, true))
+		println("\n   average number of RBIs for batter(3): "+ imp2.mean(b3tRBI))
+		println("\n   average number of RBIs for batter(4): "+ imp2.mean(b4tRBI))
+		println("\n   average number of RBIs for batter(5): "+ imp2.mean(b5tRBI))
+	  	println("\n############################################################\n")
+	  	imp2.kill()
+
+	  	team2Wins.unobserve()
+
+  //       println("############################################################")
+		// val mpeMH = MetropolisHastingsAnnealer(100000, ProposalScheme.default, Schedule.default(1.0))
+		// println("MPE Metropolis Hastings running ...")
+  //       mpeMH.start()
+  //       println("Given that team 2 has won the game, what's the most probable value of: ")
+  //       println("Batter3's RBI: " + mpeMH.mostLikelyValue(b3tRBI))
+  //       mpeMH.kill()
+
+  //      	team2Wins.unobserve()
+	}
+
 	def main(args: Array[String]) {
 		checkArgsLength(args)
 		createDatabases()
 		checkValidNames(args)
 		initializeProbs()
-
 		val triple = playGame(3)
-
-		for {
-			result <- triple
-		}
-		yield {
-			val (result1, result2, result3) = result
-			printBoxScore(result1, result2)
-		}
-
-		val team2Wins = triple._3 
-		val algorithm = Importance(100, team2Wins)
-		algorithm.start()
-		println("Win Probability of team 2, when the game is tied, and bases "
-				+ "are loaded with 1 out : " + algorithm.probability(team2Wins, true))
-		algorithm.kill()
+		runAlgorithms(triple)
 	}
 }
 
